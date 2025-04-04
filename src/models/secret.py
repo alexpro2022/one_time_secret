cc06964fef238cafc71213046a216a843d73400a
Alex Proskuriakov <alexpro1972@yandex.ru>
2025-04-05 00:00:37 +0300

    Initial

--- src/models/secret.py
+++ src/models/secret.py
@@ -2,7 +2,6 @@ from src.repo.models.base import Base, Mapped, mapped_column
 
 
 class Secret(Base):
-    name: Mapped[str] = mapped_column(index=True, unique=True)
-    secret_name: Mapped[str]
-    age: Mapped[int | None]
-    new_field: Mapped[str] = mapped_column(default="To update manually")
+    secret: Mapped[str]  # = mapped_column(index=True, unique=True)
+    passphrase: Mapped[str | None]
+    ttl_seconds: Mapped[int | None]
--- src/repo/models/__init__.py
+++ src/repo/models/__init__.py
@@ -1,6 +1,4 @@
 """Import all your models here. It is necssary for the alembic migrations."""
 
 from src.repo.models.base import Base  # noqa
-
-# Below is for example
 from src.models.secret import Secret  # noqa
